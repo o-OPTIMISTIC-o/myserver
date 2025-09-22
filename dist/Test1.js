18e1cbd0ed4d1eeb47d58d00d733ffa96b4dec31
tanakorn-kanoup <thanakorn0963363144@gmail.com>
2025-09-22 14:40:13 +0700

    add test1

---
+++ dist/Test1.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Utils_1 = require("./Utils");
+if ((0, Utils_1.add)(2, 3) === 5) {
+    console.log(0);
+}
+else {
+    console.log(1);
+}
---
+++ dist/Utils.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloworld = helloworld;
+exports.add = add;
+function helloworld() {
+    return "Hello, World!";
+}
+function add(a, b) {
+    return a + b;
+}
