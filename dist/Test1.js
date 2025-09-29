0588432a2df7a3c9e513d755bffad9f83a086d34
tanakorn-kanoup <thanakorn0963363144@gmail.com>
2025-09-29 15:23:24 +0700

    commit

--- dist/Test1.js
+++ dist/Test1.js
@@ -1,9 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Utils_1 = require("./Utils");
-if ((0, Utils_1.add)(2, 3) === 5) {
-    console.log(0);
-}
-else {
-    console.log(1);
-}
+const unit_test = () => {
+    // test case 1
+    if ((0, Utils_1.add)(2, 3) === 5) {
+    }
+    else {
+        console.log(1);
+        return;
+    }
+    // test case 2
+    if ((0, Utils_1.add)(-2, -3) === -5) {
+    }
+    else {
+        console.log(2);
+        return;
+    }
+};
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,27 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const mongoose_1 = __importDefault(require("mongoose"));
+const UserRoutes_1 = __importDefault(require("./UserRoutes"));
+const cors_1 = __importDefault(require("cors"));
+const path = require("path");
 const app = express();
+const mongoURI = 'mongodb+srv://tanakorn_kanoup:Copy_2548@cluster0.fhntsui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose_1.default
+    .connect(mongoURI)
+    .then(() => {
+    console.log('MongoDB connected');
+})
+    .catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
+app.use(express.json());
+app.use((0, cors_1.default)());
+app.use(express.static(path.join(__dirname, "src", 'public')));
+app.use('/api', UserRoutes_1.default);
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
